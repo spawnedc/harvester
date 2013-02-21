7af093cb5e3265a0515a2bddf1a9f2a760000726
Luke Benstead <lukeb@potatolondon.com>
2013-02-21 12:24:01 +0000

    Update app.yaml to work with the new submodules

--- testapp/wsgi.py
+++ testapp/wsgi.py
@@ -20,8 +20,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testapp.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from djangoappengine.main import application
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
