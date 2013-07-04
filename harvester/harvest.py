4a5c4371a18f276402d2fc74dce15c3af1793fdb
Can Yilmaz <can@potatolondon.com>
2013-07-04 17:27:29 +0100

    Reinventing the wheel

--- harvester/harvest.py
+++ harvester/harvest.py
@@ -18,12 +18,16 @@ class Harvest(object):
     def get_api_response(self, url, **kwargs):
         try:
             result = urlfetch.fetch("%s/%s" % (self.url, url), headers=self.headers, deadline=10, **kwargs)
+            logging.critical(result.content)
             return json.loads(result.content)
         except urlfetch.DeadlineExceededError:
             return None
 
     def _get_user(self):
         return self.get_api_response('account/who_am_i')
+
+    def get_project(self, project_id):
+        return self.get_api_response('projects/%s' % project_id)
 
     def get_report(self, from_date, to_date):
         url = 'people/%s/entries?from=%s&to=%s' % (self.user_id, from_date, to_date)
@@ -35,6 +39,9 @@ class Harvest(object):
         for entry in report:
             hours = float(entries.get(entry.get('day_entry').get('project_id'), 0))
             hours += float(entry.get('day_entry').get('hours'))
+
+            # project = self.get_project(entry.get('day_entry').get('project_id'))
+            # entries[project.get('name')] = hours
             entries[entry.get('day_entry').get('project_id')] = hours
 
         for (project_id, hours) in entries.iteritems():
